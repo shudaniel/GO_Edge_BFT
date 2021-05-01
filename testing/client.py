4569db42f83b9ed9472a6a5b7cd5679eae187b4b
Daniel Shu <danielshu98@gmail.com>
2021-04-30 19:41:37 -0700

    fix client

--- testing/client.py
+++ testing/client.py
@@ -6,7 +6,7 @@ import time
 import json
 
 
-def client_thread(id, ip, port, num_transactions, percent, zone, primaries, times, start_times, sem):
+def client_thread(id, ip, port, num_transactions, percent, zone, primaries, times, sem):
     new_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     new_sock.bind((ip, port))
     client_id = ip + ":" + str(port) + ":" + str(id)
@@ -21,40 +21,32 @@ def client_thread(id, ip, port, num_transactions, percent, zone, primaries, time
             print(ip_addr, port)
             sock.sendto(clientjoin_msg.encode('utf-8'), (ip_addr, int(port)))
         
-    num_global = 0
     sem.acquire()
 
-    num_local = 0
-    clientstart = time.time()
     for i in range(num_transactions):
         random_number = random.random()
         new_txn = "CLIENT_REQUEST|" + str(client_id) + "!" + str(i) + "!10*" 
         msgtype = ""
-        start = time.time()
         if random_number <= percent:
             # Do a global sync
             # print("Global")
             # print("Sending global:", new_txn)
             new_sock.sendto(new_txn.encode('utf-8'), (primaries["0"][0], int(primaries["0"][1])))
-            num_global += 1
             msgtype = "g"
         else:
             # print("Local")
             # Do a local trans
             # print("Sending local:", new_txn)
             new_sock.sendto(new_txn.encode('utf-8'), (primaries[zone][0], int(primaries[zone][1])))
-            num_local += 1
+           
             msgtype = "l"
         data, addr = new_sock.recvfrom(1024)
-        end = time.time()
-        total_time = end - start
 
         reply = data.decode()[:-1]
-        print("Received reply", reply, total_time, msgtype)
+        print("Received reply", reply, msgtype)
         
         if reply != "FAILURE":
             times.append(float(reply))
-    start_times.append(clientstart)
 
 
 parser = argparse.ArgumentParser()
@@ -65,7 +57,6 @@ parser.add_argument("--numclients", "-c", type=int, default=1)
 parser.add_argument("--numtransactions", "-t", type=int, default=10)
 parser.add_argument("--percent", "-r", type=float, default=0.1)
 
-starttimes = []
 times = []
 args = parser.parse_args()
 client_id_counter = 0
@@ -85,7 +76,7 @@ with open("primaries.json", "r") as readfile:
 for i in range(num_clients):
     sem = threading.Semaphore(0)
     semaphores.append(sem)
-    new_thread = threading.Thread(target=client_thread, args=(i, args.address, args.port + i + 1, num_transactions, args.percent, args.zone, primaries, times, starttimes, sem))
+    new_thread = threading.Thread(target=client_thread, args=(i, args.address, args.port + i + 1, num_transactions, args.percent, args.zone, primaries, times, sem))
     client_threads.append(new_thread)
 
 # start = input("input")
@@ -107,6 +98,6 @@ times_sum = 0
 for time in times:
     times_sum += time
 
-final_msg = str(times_sum) + ";" + str(min(starttimes)) + ";" + str(len(times)) + "|"
+final_msg = str(times_sum) + "|-------|" + str(len(times)) + "|*"
 sock.sendto(final_msg.encode('utf-8'), addr)
 print("Done")
